71117db7c7e7e1f4f04cc8dd2dd190a4e6b93bfd
Ihor Porokhnenko <ihor.porokhnenko@gmail.com>
2021-12-21 14:26:25 +0200

    add project files

---
+++ web/app/Api/V1/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @var Laravel\Lumen\Routing\Router $router
+ */
+$router->group([
+    'prefix' => env('APP_API_VERSION', ''),
+    'namespace' => '\App\Api\V1\Controllers',
+    'middleware' => 'checkUser'
+], function ($router) {
+
+    /**
+     * ADMIN PANEL
+     */
+    $router->group([
+        'prefix' => 'admin',
+        'namespace' => 'Admin',
+        'middleware' => 'checkAdmin'
+    ], function ($router) {
+    });
+});
