6a8c7fb222b1cd6f03b9a1aa4f1249f6f2d60810
Ihor Porokhnenko <ihor.porokhnenko@gmail.com>
2022-02-16 13:00:28 +0200

    add sumra sdk, remove unneeded files

--- web/app/Api/V1/Controllers/LineController.php
+++ web/app/Api/V1/Controllers/LineController.php
@@ -1,21 +1,23 @@
 <?php
 
 namespace App\Api\V1\Controllers;
-use LINE\LINEBot;
+
 use Illuminate\Http\Response;
-use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+use LINE\LINEBot;
+
 class LineController extends Controller
 {
-	public function __construct(){
-		
-		/*$this->channelAccessToken = Setting::getChannelAccessToken();
-		$this->channelSecret = Setting::getChannelSecret();
-		$this->apiReply = Setting::getApiReply();
-		$this->apiPush = Setting::getApiPush();
-		$this->webhookResponse = file_get_contents('php://input');
-		$this->webhookEventObject = json_decode($this->webhookResponse);*/
-	}
-	 /**
+    public function __construct()
+    {
+        /*$this->channelAccessToken = Setting::getChannelAccessToken();
+        $this->channelSecret = Setting::getChannelSecret();
+        $this->apiReply = Setting::getApiReply();
+        $this->apiPush = Setting::getApiPush();
+        $this->webhookResponse = file_get_contents('php://input');
+        $this->webhookEventObject = json_decode($this->webhookResponse);*/
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @OA\Get(
@@ -98,47 +100,44 @@ class LineController extends Controller
      *         description="Not found"
      *     )
      * )
-    /**
+     * /**
      * Create a new controller instance.
      *
      * @return void
      */
     public function index(Response $response)
     {
-	
-	// get request body and line signature header
-	$body 	   = file_get_contents('php://input');
-	//$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+        // get request body and line signature header
+        $body = file_get_contents('php://input');
+        //$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 
-	// log body and signature
-	file_put_contents('php://stderr', 'Body: '.$body);
+        // log body and signature
+        file_put_contents('php://stderr', 'Body: ' . $body);
 
-	// is LINE_SIGNATURE exists in request header?
-	/*if (empty($signature)){
-		return $response->withStatus(400, 'Signature not set');
-	}
+        // is LINE_SIGNATURE exists in request header?
+        /*if (empty($signature)){
+            return $response->withStatus(400, 'Signature not set');
+        }
 
-	// is this request comes from LINE?
-	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
-		return $response->withStatus(400, 'Invalid signature');
-	}*/
+        // is this request comes from LINE?
+        if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+            return $response->withStatus(400, 'Invalid signature');
+        }*/
 
-	// init bot
-	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
-	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
-	$data = json_decode($body, true);
-	foreach ($data['events'] as $event)
-	{
-		$userMessage = $event['message']['text'];
-		if(strtolower($userMessage) == 'hello')
-		{
-			$message = "Pradeep";
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
-			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-		
-		}
-	}
-	}
-	
+        // init bot
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(env('LINE_BOT_CHANNEL_ACCESS_TOKEN'));
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_BOT_CHANNEL_SECRET')]);
+        $data = json_decode($body, true);
+
+        foreach ($data['events'] as $event) {
+            $userMessage = $event['message']['text'];
+            if (strtolower($userMessage) == 'hello') {
+                $message = "Pradeep";
+                $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+                $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+                return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+
+            }
+        }
+    }
 }
