2d8aa96eabef60b52455f665932ca6537e16130b
Dhana <ihor.porokhnenko@gmail.com>
2022-04-12 17:33:31 +0300

    any update and changes

--- web/app/Api/V1/routes.php
+++ web/app/Api/V1/routes.php
@@ -5,22 +5,26 @@
  */
 $router->group([
     'prefix' => env('APP_API_VERSION', ''),
-    'namespace' => '\App\Api\V1\Controllers',
-    //'middleware' => 'checkUser'
+    'namespace' => '\App\Api\V1\Controllers'
 ], function ($router) {
     /**
-     * Channels Auth
+     * Internal access
      */
     $router->group([
-        'prefix' => 'channels',
+        'middleware' => 'checkUser'
     ], function ($router) {
-        $router->get('/auth/{platform}', 'ChannelController');
+        /**
+         * Channels Auth
+         */
+        $router->group([
+            'prefix' => 'channels',
+        ], function ($router) {
+            $router->get('/auth/{platform}', 'ChannelController');
+        });
+
+        $router->get('/bot/{messengerInstance}/send-message', 'MessengerController@sendMessage')->name('send-message');
+        $router->get('/bot/{messengerInstance}/webhook', 'MessengerController@handleWebhook')->name('webhook');
     });
-
-
-    $router->get('/bot/{messengerInstance}/send-message', 'MessengerController@sendMessage')->name('send-message');
-    $router->get('/bot/{messengerInstance}/webhook', 'MessengerController@handleWebhook')->name('webhook');
-
 
     /**
      * ADMIN PANEL
@@ -28,7 +32,10 @@ $router->group([
     $router->group([
         'prefix' => 'admin',
         'namespace' => 'Admin',
-        'middleware' => 'checkAdmin',
+        'middleware' => [
+            'checkUser',
+            'checkAdmin'
+        ]
     ], function ($router) {
         /**
          * Channels (Bots)
@@ -42,7 +49,6 @@ $router->group([
             $router->put('/{id:[a-fA-F0-9\-]{36}}', 'BotController@update');
             $router->delete('/{id:[a-fA-F0-9\-]{36}}', 'BotController@destroy');
             $router->post('/{id:[a-fA-F0-9\-]{36}}/update-status', 'BotController@updateStatus');
-
         });
     });
 });
