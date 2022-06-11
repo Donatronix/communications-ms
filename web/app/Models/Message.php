e44f0a84a71bdf3a111bdddf17afb14773265d1e
Taiwo Soewu <taiwo_soewu.1995@ymail.com>
2022-06-11 17:52:46 +0100

    added user model and migration with the messenger congif

--- web/app/Models/Message.php
+++ web/app/Models/Message.php
@@ -7,22 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Sumra\SDK\Traits\UuidTrait;
 
-class Messagej extends Model
+class Message extends Model
 {
     use HasFactory;
     use UuidTrait;
     use SoftDeletes;
-    
-    const STATUS_PROCESSING = 1;
-    const STATUS_QUEUE_FAIL = 2;
-    const STATUS_SENT = 3;
 
     protected $fillable = [
-        'sender_user_id',
-        'subject',
-        'body',
-        'recipient_email',
-        'status',
-        'note'
+        'user_id',
     ];
  }
