c2245bcbd12942af18379ced61334d0f9cd001c6
Taiwo Soewu <taiwo_soewu.1995@ymail.com>
2022-06-17 14:32:02 +0100

    created models for bot chats and bot conversations

--- web/app/Models/BotChat.php
+++ web/app/Models/BotChat.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Sumra\SDK\Traits\UuidTrait;
 
 class BotChat extends Model
 {
-    //
+    use HasFactory;
+    use UuidTrait;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'message_id', 'date', 'text', 'replied_to_message_id', 'bot_conversation_id'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * One Chat belongs to one Conversation
+     *
+     * @return BelongsTo
+     */
+    public function botconversation(): BelongsTo
+    {
+        return $this->belongsTo(BotConversation::class);
+    }
 }
--- web/app/Models/BotConversation.php
+++ web/app/Models/BotConversation.php
@@ -2,9 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Sumra\SDK\Traits\UuidTrait;
 
 class BotConversation extends Model
 {
-    //
+    use HasFactory;
+    use UuidTrait;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 'bot_name', 'bot_username', 'chat_id', 'first_name', 'last_name'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    /**
+     * One Conversation has many Chats
+     *
+     * @return HasMany
+     */
+    public function botchats(): HasMany
+    {
+        return $this->hasMany(BotChat::class);
+    }
 }
